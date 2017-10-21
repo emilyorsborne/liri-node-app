321dda95fca8ebd6854c044830dcc15da40c2ec2
emilyorsborne <emorsborne@gmail.com>
2017-10-21 10:24:24 -0400

    tomatoes

--- liri.js
+++ liri.js
@@ -3,8 +3,9 @@ var request = require("request");
 var movieName = process.argv[2];
 
 // OMDB API request 
-var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece";
+var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&tomatoes=true&apikey=40e9cece";
 
+// var queryUrl = "http://www.omdbapi.com/?t=Mr.Nobody&y=&plot=short&tomatoes=true&apikey=40e9cece";
 
 console.log(queryUrl);
 
@@ -14,6 +15,7 @@ request(queryUrl, function(error, response, body) {
 
         var parse = JSON.parse(body, null, 2)
 
-        console.log("Title: " + parse.Title + "\nYear: " + parse.Year + "\nIMDB Rating: " + parse.imdbRating + "\nCountry: " + parse.Country + "\nLanguage: " + parse.Language + "\nPlot: " + parse.Plot + "\nActors: " + parse.Actors);
+        console.log("Title: " + parse.Title + "\nYear: " + parse.Year + "\nIMDB Rating: " + parse.imdbRating + "\nRotton Tomatoes: " + parse.tomatoRating + "\nCountry: " + parse.Country + "\nLanguage: " + parse.Language + "\nPlot: " + parse.Plot + "\nActors: " + parse.Actors);
+
     }
 });
